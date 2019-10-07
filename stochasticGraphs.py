939e62bf79cfeab26df989fa930a32a37e267667
Han Du <du.yuehan@gmail.com>
2019-10-07 17:21:35 -0400

    incrementing information propagation

--- stochasticGraphs.py
+++ stochasticGraphs.py
@@ -1,93 +1,161 @@
-# %%markdown
+%%markdown
+
 # Stochastic Processes in Graphs
-##### Han Du
-# %%markdown
+###### Han
+# %%
+%%markdown
 
 The purposes of this notebook is to study stochastic processes and algorithms on graphs.
 
-I will represent my graphs as an adjacency matrix $G$.
+We will study random graphs $G = (N, p)$ where $N$ is fixed number of vertices and $p$ represents the probability of an edge being included.
 
-$G_{i,j} = 1$ if there is an interaction between node i and node j.
+We will represent graphs as an adjacency matrix $A$ of $G$.
 
-$G_{i,i}$ represents the value stored by node i.
+$A_{i,j} = 1$ if there is an interaction between node i and node j.
+
+$A_{i,i}$ represents the value stored by node i.
 
 ## Information Propagation in a Stochastic Network from Single Source
 
 We start with studying the speed of information propagation in a graph where values of nodes are updated by the rule:
 
-$G_{i,j} = 1 \implies \max{(G_{i,i},G_{j,j})} \rightarrow G_{i,i}, G_{j,j}$
+$A_{i,j} = 1 \implies \max{(A_{i,i},A_{j,j})} \rightarrow A_{i,i}, A_{j,j}$
 
-In this section we study random graphs $G = (N, p)$ where $N$ is fixed number of vertices and $p$ represents the probability of an edge being included.
+After an adjacent node is updated, those nodes do not update adjacent nodes until the next time step or iteration of the propagation rule.
 
+### Constant information
+
+In this section we study how quickly constant information $C$ propagates in a constant sized graph $G = (N, p)$
+
+i.e. how many timesteps or iterations of the update rule it takes before $A_{i, i} = A_{j, j} \; \forall \, i,j \in E(G)$
 
 # %%
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 from functools import reduce
+import seaborn as sns
+import pandas as pd
 
-n = 10
-p = 0.5
-upperDiagIndices = np.triu_indices(n, 1)
 
 # graph initialization
 
-def randomizeEdges(G, n, p):
-    m = map((lambda x: x > p), np.random.normal(size = int((n-1) * n / 2)))
+n = 10
+p = 0.5
+
+def initializeGraph(n, p):
+    upperDiagIndices = np.triu_indices(n, 1)
+    A = np.zeros((n, n))
+    return upperDiagIndices, A
+
+def randomizeEdges(A, n, p):
+    m = map((lambda x: x < p), np.random.rand(int((n-1) * n / 2)))
     edges = np.array([1 if e else 0 for e in m])
-    G[upperDiagIndices] = edges
-    return G
+    A[upperDiagIndices] = edges
+    return A
 
 
-def randomAccumulate(G, n, p):
+def randomAccumulate(A, n, p):
     randIdx = np.random.randint(low = 0, high = n)
-    G[randIdx, randIdx] += 1
-    return G
+    A[randIdx, randIdx] += 1
+    return A
 
 
-def drawGraph(G):
-    nxG = nx.from_numpy_matrix(G)
-    labelDict = dict(zip(nxG, np.diag(G).astype(int).tolist()))
-    nx.draw(nxG, labels = labelDict)
+def drawGraph(A):
+    nxA = nx.from_numpy_matrix(A)
+    labelDict = dict(zip(nxA, np.diag(A).astype(int).tolist()))
+    nx.draw(nxA, labels = labelDict)
     ax = plt.gca()
     ax.set_aspect('equal')
 
 
-def oneStepPropagation(G):
+def oneStepPropagation(A):
     changed = []
-    for i, row in enumerate(G):
+    for i, row in enumerate(A):
         for j, column in enumerate(row):
             if j > i:
-                if G[i, j] == 1 and i not in changed and j not in changed:
-                    maxIJ = max(G[i, i], G[j, j])
-                    if G[i, i] != maxIJ:
+                if A[i, j] == 1 and i not in changed and j not in changed:
+                    maxIJ = max(A[i, i], A[j, j])
+                    if A[i, i] != maxIJ:
                         changed.append(i)
                     else:
                         changed.append(j)
-                    G[i, i] = maxIJ
-                    G[j, j] = maxIJ
+                    A[i, i] = maxIJ
+                    A[j, j] = maxIJ
+    return A
     # print(changed)
 
 
-def checkAllEqual(G):
-    return reduce((lambda x, y: x == y), np.diag(G))
+def checkAllEqual(A):
+    return (np.diag(A) == A[0,0]).all()
 
 queue = []
 iterations = 100000
 for i in range(iterations):
     count = 0
-    G = np.zeros((n, n))
-    G = randomizeEdges(G, n, p)
-    G = randomAccumulate(G, n, p)
-    # print(G)
-    while not checkAllEqual(G):
-        G = randomizeEdges(G, n, p)
-        oneStepPropagation(G)
-        # print(G)
+    upperDiagIndices, A = initializeGraph(n, p)
+    A = randomizeEdges(A, n, p)
+    A = randomAccumulate(A, n, p)
+    # print(A)
+    while not checkAllEqual(A):
+        A = randomizeEdges(A, n, p)
+        A = oneStepPropagation(A)
+        # print(A)
         count +=1
     queue.append(count)
     count = 0
 
 average = sum(queue)/iterations
 print("It takes on average " + str(average) + " iterations until the entire network has updated in value.")
+sns.countplot(list(map(int, queue)))
+# %%
+%%markdown
+### Incrementing information
+
+Here we introduce a new concept of incrementing information:
+
+i.e. $C_t \geq C_{t-1} \; \forall t$
+
+Where $t$ is time and $C_t$ is determined by an arbitrary monotonically increasing function $f(t)$
+
+We measure the lag $L$ by:
+$$L(G_t) = \sum_{i}^{N}{C_t - A_{i,i}}$$
+
+For our simulation, we select $f(t) = t$
+#%%
+
+def randomAssign(A, n, p, t):
+    randIdx = np.random.randint(low = 0, high = n)
+    A[randIdx, randIdx] = t
+    return A
+
+def lag(A, t):
+    return sum(np.ones(np.diag(A).shape) * t - np.diag(A))
+
+
+def f(timestep):
+    return timestep
+
+maxTime = 50
+iterations = 1000
+n = 10
+p = 1
+lags = np.zeros((iterations, maxTime))
+for iter in range(iterations):
+    count = 0
+    upperDiagIndices, A = initializeGraph(n, p)
+    A = randomizeEdges(A, n, p)
+    # print(A)
+    while count < maxTime:
+        A = oneStepPropagation(A)
+        A = randomizeEdges(A, n, p)
+        A = randomAssign(A, n, p, count)
+        lags[iter, count] = lag(A, count)
+        count += 1
+    count = 0
+
+sns.lineplot(data=pd.DataFrame(lags).mean())
+#
+# average = sum(queue)/iterations
+# print("It takes on average " + str(average) + " iterations until the entire network has updated in value.")
 # %%
